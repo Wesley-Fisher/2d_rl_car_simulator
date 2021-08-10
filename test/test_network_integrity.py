78dc65b2cef313b4e03b3c4be31b47b5c222de38
Wesley Fisher <wesley_b_fisher@hotmail.com>
2021-08-10 19:43:41 -0300

    Add tests for failed loading

--- test/test_network_integrity.py
+++ test/test_network_integrity.py
@@ -188,7 +188,47 @@ class TestNetworkBasics(unittest.TestCase):
             v0 = net.model(s_net, net.frozen_model)[0][2]
             self.assertTrue(True)
 
-                
+    def test_failed_load_freeze_repeated(self):
+        settings = Settings()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        experience_file = dir_path + "/temp_data/memory/experience.pk"
+        model_file = dir_path + "/temp_data/memory/model.h5"
+        settings._files.root_dir = dir_path + "/temp_data"
+        settings.memory.load_saved_network = True
+        settings.memory.load_saved_experience = True
+
+        world = WorldCreation(settings).get()
+        preprocessor = ExperiencePreprocessor(settings)
+        experience = ExperienceEngine(settings, world, preprocessor)
+        physics = PhysicsEngine(settings, world, experience)
+        cs = CarState()
+        car = Car(settings, cs)
+        s0 = physics.get_car_state(car)
+        net = Network(settings, len(s0))
+
+        s_net = np.array(s0).reshape((1,len(s0)))
+        for i in range(0, 3):
+            try:
+                os.remove(experience_file)
+            except Error as e:
+                pass
+
+            try:
+                os.remove(model_file)
+            except Error as e:
+                pass
+            time.sleep(0.1)
+
+            self.assertFalse(os.path.isfile(experience_file))
+            self.assertFalse(os.path.isfile(model_file))
+            self.assertFalse(net.load_state())
+            net.freeze()
+
+            v0 = net.get(s_net)[0][2]
+            v0 = net.model(s_net)[0][2]
+            v0 = net.model(s_net, net._model)[0][2]
+            v0 = net.model(s_net, net.frozen_model)[0][2]
+            self.assertTrue(True)                
 
 
 if __name__ == '__main__':
